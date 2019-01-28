29e5e8174def117b37e70456f51c580eba50cea0
reo.koide <reo.koide@ge-creative.co.jp>
2019-01-28 17:52:54 +0900

    ルート変更 kintoneレコード登録

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 let mainWindow = null;
 
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+// Telloドローン IP,PORT
+var PORT = 8889;
+var HOST = '192.168.10.1';
+
+var dgram = require('dgram');
+var client = dgram.createSocket('udp4');
 
 // Express
 const express = require("express");
@@ -42,50 +52,226 @@ app.on("ready", () => {
 
 
 /*** scratchデータの受取 **/
-exapp.get('/test/:volume/:id/:number',(req, res) => {
-    console.log(req.params)
-    /** scratchデータの取得 **/
-    var volume = req.params.volume;
-    var id = req.params.id;
-    var number = req.params.number;
+exapp.get('/',(req, response) => {
 
-    /** kintoneへアクセス **/
-    var entry_body = {
-        'app': 350,
-        'record':{
-            'test_one':{
-                "value": volume
-            },
-            'test_two': {
-                "value": id
-            },
-            'test_three': {
-                "value": number
-            }
-        }
-    };
+    var pathname = url.parse(req.url).pathname;
+       
+    var url_params = req.url.split('/');
 
-    let params = {
-        url:'https://ge-creative.cybozu.com/k/v1/record.json',
-        method: 'POST',
-        json: true,
-        headers: {
-            'X-Cybozu-API-Token': 'vCLeMxYChZoBHjai5eHyLPvtbTmjWcHGrXAH7KEm',
-            'Content-Type': 'application/json',
-        },
-        body: entry_body
-    };
+    if (url_params.length < 2)
+        return;
 
-    request(params, function(err, resp, body) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log(body);
-    });
+    var command = url_params[1];
+	
+	  switch (command){
+		
+        case 'poll':
+            respondToPoll(response);
+            break;
+		
+        case 'takeoff':
+			console.log('takeoff');
+			TakeoffRequest();
+		break;
+		
+        case 'land':
+			console.log('land');
+			LandRequest();
+		break;
+		
+        case 'up':
+			dis = (url_params.length >= 3) ? url_params[2] : 0;
+			console.log('up ' + dis);
+			var message = new Buffer( 'up '+ dis );
+			client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+				if (err) throw err;
+			});
+		break;
 
-    res.send("OK");
+        case 'down':
+			dis = (url_params.length >= 3) ? url_params[2] : 0;
+			console.log('down ' + dis);
+			var message = new Buffer( 'down '+ dis );
+			client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+				if (err) throw err;
+			});			
+		break;
+
+        case 'left':
+			dis = (url_params.length >= 3) ? url_params[2] : 0;
+			console.log('left ' + dis);
+			var message = new Buffer( 'left '+ dis );
+			client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+				if (err) throw err;
+			});
+		break;
+
+        case 'right':
+			dis = (url_params.length >= 3) ? url_params[2] : 0;
+			console.log('right ' + dis);
+			var message = new Buffer( 'right '+ dis );
+			client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+				if (err) throw err;
+			});
+		break;		
+		
+		case 'forward':
+			dis = (url_params.length >= 3) ? url_params[2] : 0;
+			console.log('forward ' + dis);
+			var message = new Buffer( 'forward '+ dis );
+			client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+				if (err) throw err;
+			});			
+		break;		
+		
+        case 'back':
+			dis = (url_params.length >= 3) ? url_params[2] : 0;
+			console.log('back ' + dis);
+			var message = new Buffer( 'back '+ dis );
+			client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+				if (err) throw err;
+			});			
+		break;
+
+        case 'cw':
+			dis = (url_params.length >= 3) ? url_params[2] : 0;
+			console.log('cw ' + dis);
+			var message = new Buffer( 'cw '+ dis );
+			client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+				if (err) throw err;
+			});
+		break;
+
+		case 'flip':
+			dis = (url_params.length >= 3) ? url_params[2] : 0;
+			console.log('flip' + dis);
+			var message = new Buffer( 'flip '+ dis );
+			client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+				if (err) throw err;
+			});			
+		break;	
+
+		case 'ccw':
+			dis = (url_params.length >= 3) ? url_params[2] : 0;
+			console.log('ccw ' + dis);
+			var message = new Buffer( 'ccw '+ dis );
+			client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+				if (err) throw err;	
+			});
+			client.on('message',function(msg,info){
+				console.log('Data received from server : ' + msg.toString());
+				console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+			});								
+		break;		
+		
+		case 'setspeed':
+			dis = (url_params.length >= 3) ? url_params[2] : 0;
+			console.log('setspeed ' + dis);
+			var message = new Buffer( 'speed '+ dis );
+			client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+				if (err) throw err;
+			});			
+        break;
+
+        case 'temp':
+            var temp = req.params;
+            console.log(temp);
+			// var message = new Buffer( 'speed '+ dis );
+			// client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+			// 	if (err) throw err;
+			// });			
+        break;
+
+        case 'height':
+            var height = req.params;
+            console.log(height);
+			// var message = new Buffer( 'speed '+ dis );
+			// client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+			// 	if (err) throw err;
+			// });			
+        break;
+        
+        case 'kintoneAddRecord':
+            console.log(temp);
+            console.log(height);
+            /** kintoneへアクセス **/
+            var entry_body = {
+                'app': 350,
+                'record':{
+                    'test_one':{
+                        "value": volume
+                    },
+                    'test_two': {
+                        "value": id
+                    },
+                    'test_three': {
+                        "value": number
+                    }
+                }
+            };
+
+            let params = {
+                url:'https://ge-creative.cybozu.com/k/v1/record.json',
+                method: 'POST',
+                json: true,
+                headers: {
+                    'X-Cybozu-API-Token': 'vCLeMxYChZoBHjai5eHyLPvtbTmjWcHGrXAH7KEm',
+                    'Content-Type': 'application/json',
+                },
+                body: entry_body
+            };
+
+            request(params, function(err, res, body) {
+                if (err) {
+                console.log(err);
+                return;
+                }
+                console.log(body);
+            });
+        break;
+			
+	  }
+    response.end('Hello Tello.\n');
+
 });
+
+
+function respondToPoll(response){
+
+    var noDataReceived = false;
+
+    var resp = "";
+    var i;
+    for (i = 0; i < dataToTrack_keys.length; i++){
+        resp += dataToTrack_keys[i] + " ";
+        resp += (i+10);
+		resp += "\n";
+    }
+    response.end(resp);
+}
+
+function TakeoffRequest(){
+	
+	var message = new Buffer('command');
+
+	client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+		if (err) throw err;
+	});
+	var message = new Buffer('takeoff');
+	client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+		if (err) throw err;
+
+	});
+}
+
+function LandRequest(){
+
+	var message = new Buffer('land');
+
+	client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+		if (err) throw err;
+	});
+}
 
 
 
