1c0104d03f806702a811f805ce1548bb220bd8b2
reo.koide <reo.koide@ge-creative.co.jp>
2019-01-30 17:18:26 +0900

    responseデータをkintoneへ登録

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 "use strct";
 
-
-var dataToTrack_keys = ["battery",""];
-var lastDataReceived = null;
-
 // Electron
 const electron = require("electron");
 const app = electron.app;
@@ -11,7 +7,7 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow = null;
 
 const http = require('http');
-const request = require('request');
+const kintoneRequest = require('request');
 const fs = require('fs');
 const url = require('url');
 
@@ -20,153 +16,82 @@ const client = dgram.createSocket('udp4');
 
 
 
+// Telloドローン IP,PORT
+let PORT = 8889;
+let HOST = '192.168.10.1';
+
+let dataToTrack_keys = ["speed", "battery", "fly_time", "height", "temperature", "tof", "acceleration"];
+let lastDataReceived = null;
+
+
 // イベント：起動時
 app.on("ready", () => {
-    // ウィンドウサイズを1280*720（フレームサイズを含まない）に設定する
-    mainWindow = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        useContentSize: true
-    });
-    // 使用するhtmlファイルを指定する
-    mainWindow.loadURL(`file://${__dirname}/views/index.html`);
-
-    // 開発者ツールを表示する
-    mainWindow.webContents.openDevTools();
-
-    // ローカルホストを画面描画
-    mainWindow.loadURL("http://127.0.0.1:8001");
-
-    // イベント：ウィンドウクローズ時
-    mainWindow.on("closed", () => {
-        // 以下の処理実行時、イベント：全てのウィンドウが閉じた場合が実行されます
-        mainWindow = null;
+	// ウィンドウサイズを1280*720（フレームサイズを含まない）に設定する
+	mainWindow = new BrowserWindow({
+		width: 300,
+		height: 300,
+		useContentSize: true
 	});
-	
-	http.createServer( function (req, response) {
-		var pathname = url.parse(req.url).pathname;
-	
-		var url_params = req.url.split('/');
-	
+	// 使用するhtmlファイルを指定する
+	mainWindow.loadURL(`file://${__dirname}/views/index.html`);
+
+	// 開発者ツールを表示する
+	mainWindow.webContents.openDevTools();
+
+	// イベント：ウィンドウクローズ時
+	mainWindow.on("closed", () => {
+		// 以下の処理実行時、イベント：全てのウィンドウが閉じた場合が実行されます
+		mainWindow = null;
+	});
+
+
+	http.createServer(function (request, response) {
+
+		var pathname = url.parse(request.url).pathname;
+
+		var url_params = request.url.split('/');
+
 		if (url_params.length < 2)
 			return;
-			
-		// リクエストのvalue値の取得
+
 		var command = url_params[1];
 
-		switch (command){
-			
+		switch (command) {
+
 			case 'poll':
 				respondToPoll(response);
 				break;
-			
+
 			case 'takeoff':
 				console.log('takeoff');
 				TakeoffRequest();
 				break;
-			
+
 			case 'land':
 				console.log('land');
 				LandRequest();
 				break;
-			
-			case 'up':
-				dis = (url_params.length >= 3) ? url_params[2] : 0;
-				console.log('up ' + dis);
-				var message = new Buffer( 'up '+ dis );
-				client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-					if (err) throw err;
-				});
-				break;
-	
-			case 'down':
-				dis = (url_params.length >= 3) ? url_params[2] : 0;
-				console.log('down ' + dis);
-				var message = new Buffer( 'down '+ dis );
-				client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-					if (err) throw err;
-				});			
-				break;
-	
-			case 'left':
-				dis = (url_params.length >= 3) ? url_params[2] : 0;
-				console.log('left ' + dis);
-				var message = new Buffer( 'left '+ dis );
-				client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-					if (err) throw err;
-				});
-				break;
-	
-			case 'right':
-				dis = (url_params.length >= 3) ? url_params[2] : 0;
-				console.log('right ' + dis);
-				var message = new Buffer( 'right '+ dis );
-				client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-					if (err) throw err;
-				});
-				break;		
-			
-			case 'forward':
-				dis = (url_params.length >= 3) ? url_params[2] : 0;
-				console.log('forward ' + dis);
-				var message = new Buffer( 'forward '+ dis );
-				client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-					if (err) throw err;
-				});			
-				break;		
-			
-			case 'back':
-				dis = (url_params.length >= 3) ? url_params[2] : 0;
-				console.log('back ' + dis);
-				var message = new Buffer( 'back '+ dis );
-				client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-					if (err) throw err;
-				});			
-				break;
-	
-			case 'cw':
-				dis = (url_params.length >= 3) ? url_params[2] : 0;
-				console.log('cw ' + dis);
-				var message = new Buffer( 'cw '+ dis );
-				client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-					if (err) throw err;
-				});
-				break;
-	
-			case 'flip':
-				dis = (url_params.length >= 3) ? url_params[2] : 0;
-				console.log('flip' + dis);
-				var message = new Buffer( 'flip '+ dis );
-				client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-					if (err) throw err;
-				});			
-				break;	
-	
-			case 'ccw':
-				dis = (url_params.length >= 3) ? url_params[2] : 0;
-				console.log('ccw ' + dis);
-				var message = new Buffer( 'ccw '+ dis );
-				client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-					if (err) throw err;	
-				});
-				client.on('message',function(msg,info){
-					console.log('Data received from server : ' + msg.toString());
-					console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
-				});								
-				break;		
-			
-			case 'setspeed':
-				dis = (url_params.length >= 3) ? url_params[2] : 0;
-				console.log('setspeed ' + dis);
-				var message = new Buffer( 'speed '+ dis );
-				client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-					if (err) throw err;
-				});			
-				break;
-			
-			/**** kintoneアクセス ****/
+
 			case 'kintone':
 				console.log("success")
+
+				var respPoll = respondToPoll(response);
+				/**** レスポンスデータの受け取り ****/
+				// 速度
+				const telloSpeed = respPoll.respData[0]
+				// バッテリー
+				const telloBattery = respPoll.respData[1]
+				// 飛行時間
+				const flyTime = respPoll.respData[2]
+				// 高度
+				const flyHeight = respPoll.respData[3]
+				// 気温
+				const temperature = respPoll.respData[4]
+				// TOFからの距離
+				const telloTof = respPoll.respData[5]
+				// 加速度
+				const telloAcceleration = respPoll.respData[6]
+
 
 				/** kintoneへアクセス **/
 				var entry_body = {
@@ -195,7 +120,7 @@ app.on("ready", () => {
 				        }
 				    }
 				};
-	
+
 				let params = {
 				    url:'https://ge-creative.cybozu.com/k/v1/record.json',
 				    method: 'POST',
@@ -206,42 +131,138 @@ app.on("ready", () => {
 				    },
 				    body: entry_body
 				};
-	
-				request(params, function(err, res, body) {
+
+				kintoneRequest(params, function(err, res, body) {
 				    if (err) {
 				    console.log(err);
 				    return;
 				    }
 				    console.log(body);
 				});
-				
+
 				break;
+
+			case 'up':
+				dis = (url_params.length >= 3) ? url_params[2] : 0;
+				console.log('up ' + dis);
+				var message = new Buffer('up ' + dis);
+				client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+					if (err) throw err;
+				});
+				break;
+
+			case 'down':
+				dis = (url_params.length >= 3) ? url_params[2] : 0;
+				console.log('down ' + dis);
+				var message = new Buffer('down ' + dis);
+				client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+					if (err) throw err;
+				});
+				break;
+
+			case 'left':
+				dis = (url_params.length >= 3) ? url_params[2] : 0;
+				console.log('left ' + dis);
+				var message = new Buffer('left ' + dis);
+				client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+					if (err) throw err;
+				});
+				break;
+
+			case 'right':
+				dis = (url_params.length >= 3) ? url_params[2] : 0;
+				console.log('right ' + dis);
+				var message = new Buffer('right ' + dis);
+				client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+					if (err) throw err;
+				});
+				break;
+
+			case 'forward':
+				dis = (url_params.length >= 3) ? url_params[2] : 0;
+				console.log('forward ' + dis);
+				var message = new Buffer('forward ' + dis);
+				client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+					if (err) throw err;
+				});
+				break;
+
+			case 'back':
+				dis = (url_params.length >= 3) ? url_params[2] : 0;
+				console.log('back ' + dis);
+				var message = new Buffer('back ' + dis);
+				client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+					if (err) throw err;
+				});
+				break;
+
+			case 'cw':
+				dis = (url_params.length >= 3) ? url_params[2] : 0;
+				console.log('cw ' + dis);
+				var message = new Buffer('cw ' + dis);
+				client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+					if (err) throw err;
+				});
+				break;
+
+			case 'flip':
+				dis = (url_params.length >= 3) ? url_params[2] : 0;
+				console.log('flip' + dis);
+				var message = new Buffer('flip ' + dis);
+				client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+					if (err) throw err;
+				});
+				break;
+
+			case 'ccw':
+				dis = (url_params.length >= 3) ? url_params[2] : 0;
+				console.log('ccw ' + dis);
+				var message = new Buffer('ccw ' + dis);
+				client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+					if (err) throw err;
+				});
+				client.on('message', function (msg, info) {
+					console.log('Data received from server : ' + msg.toString());
+					console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port);
+				});
+				break;
+
+			case 'setspeed':
+				dis = (url_params.length >= 3) ? url_params[2] : 0;
+				console.log('setspeed ' + dis);
+				var message = new Buffer('speed ' + dis);
+				client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+					if (err) throw err;
+				});
+				break;
+
 		}
 		response.end('Hello Tello.\n');
-	
+
 	}).listen(8001);
 
 	console.log('---------------------------------------');
 	console.log('Tello Scratch Ext running at http://127.0.0.1:8001/');
 	console.log('---------------------------------------');
-
 });
+
 
 function respondToPoll(response) {
 	var noDataReceived = false;
 
-	var resp = "";
+	var resp = [];
 	var i;
 	for (i = 0; i < dataToTrack_keys.length; i++) {
-		resp += dataToTrack_keys[i] + " ";
-		resp += (i + 10);
-		resp += "\n";
+		//resp += dataToTrack_keys[i] + " ";
+		resp.push(i + 10);
+		//resp += "\n";
 	}
-	response.end(resp);
+	// response.end(resp);
+	return{respData:resp};
 
-	console.log("-------------------------------------------------------------");
-	console.log(new Date());
-	console.log(resp);
+	// console.log("-------------------------------------------------------------");
+	// console.log(new Date());
+	// console.log(resp);
 }
 
 function TakeoffRequest() {
